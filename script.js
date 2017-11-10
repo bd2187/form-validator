d7fa331bfc9a925312bd7387ba4e890a298017ec
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-11-10 14:33:43 -0800

    Add additional comments

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+/*
+* Function validates form
+*
+* @param formId-- This should be the id of the form. e.g. "bio"
+*
+* @return
+*/
+
 function validateForm(formId) {
+  // Select DOM elements
   const formEl = document.querySelector(`#${formId}`);
   const inputEls = [...document.querySelectorAll(`#${formId} input`)];
 
+  // On form submission, iterate over the input values and check if fields are empty.
   formEl.addEventListener("submit", function(evt) {
     evt.preventDefault();
 
@@ -17,7 +27,7 @@ function validateForm(formId) {
 
     if (completedFields === inputEls.length) {
       console.log("submit form");
-      formEl.submit();
+      // formEl.submit();
     } else {
       console.log("form incomplete");
     }
