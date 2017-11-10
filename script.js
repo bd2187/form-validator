2ceb3ed9aebca2847133d9900c7506bc738ff84c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-11-09 21:55:21 -0800

    Add conditional statements for submitting form

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function validateForm(formId) {
   const formEl = document.querySelector(`#${formId}`);
-  const inputEl = [...document.querySelectorAll(`#${formId} input`)];
-  var inputVals = [];
+  const inputEls = [...document.querySelectorAll(`#${formId} input`)];
 
   formEl.addEventListener("submit", function(evt) {
     evt.preventDefault();
 
-    inputEl.forEach(function(element) {
-      console.log(element.value);
+    var completedFields = 0;
+
+    inputEls.forEach(function(element) {
+      var inputValue = element.value.trim();
+
+      if (inputValue) {
+        completedFields++;
+      }
     });
+
+    if (completedFields === inputEls.length) {
+      console.log("submit form");
+      formEl.submit();
+    } else {
+      console.log("form incomplete");
+    }
   });
 }
 
