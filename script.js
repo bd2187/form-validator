a876e20744266fb177dfed33c1cc811a7f946a3e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-11-09 20:34:00 -0800

    use spread/rest for inputEl to utilize forEach method in place of for loop

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function validateForm(formId) {
   const formEl = document.querySelector(`#${formId}`);
-  const inputEl = document.querySelectorAll(`#${formId} input`);
+  const inputEl = [...document.querySelectorAll(`#${formId} input`)];
   var inputVals = [];
 
   formEl.addEventListener("submit", function(evt) {
     evt.preventDefault();
 
-    for (let i = 0; i < inputEl.length; i++) {
-      console.log(inputEl[i].value);
-    }
+    inputEl.forEach(function(element) {
+      console.log(element.value);
+    });
   });
 }
 
